4886556e22ebaf92422b1aa5052e53c75954c186
Prateek Obireddy <prateekobireddy@192-168-1-103.tpgi.com.au>
2020-02-13 19:01:47 +1100

    Organised files into index.js and renamed to App.js

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 
-export default class Example extends Component {
+class App extends Component {
     render() {
         return (
             <div className="container">
                 <div className="row justify-content-center">
                     <div className="col-md-8">
                         <div className="card">
-                            <div className="card-header">Example Component</div>
+                            <div className="card-header">React Component</div>
 
-                            <div className="card-body">I'm an example component!</div>
+                            <div className="card-body">I'm an React component!</div>
                         </div>
                     </div>
                 </div>
@@ -19,6 +18,4 @@ export default class Example extends Component {
     }
 }
 
-if (document.getElementById('example')) {
-    ReactDOM.render(<Example />, document.getElementById('example'));
-}
+export default App;
