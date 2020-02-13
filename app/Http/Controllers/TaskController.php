2db09928f9ee8cd311b8ec3d79460224cf15ea7e
Prateek Obireddy <prateekobireddy@192-168-1-103.tpgi.com.au>
2020-02-13 21:48:32 +1100

    Implemented backend index and store functions in TaskController

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Task;
+
+class TaskController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request, Task $task)
+    {
+        // get all the tasks based on current user ID
+        $allTasks = $task->whereIn('user_id', $request->user()->with('user'));
+
+        // order tasks by created at and get 20 at a time
+        $tasks = $allTasks->orderBy('created_at', 'desc')->take(20)->get();
+
+        // return response (JSON)
+        return response()->json([
+            'tasks' => $tasks,
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // Validation 
+        $this->validate($request, [
+            'name' => 'required|max:255'
+        ]);
+
+        // Create new Task based on user tasks relationship
+        $task = $request->user()->tasks()->create([
+            'name' => $request->name,
+        ]);
+
+        // return task with user object
+        return response()->json($task->with('user')->find($task->id));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
