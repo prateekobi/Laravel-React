443b84f18c9aa366ff3a07a6f4f98e0817b64fc2
Prateek Obireddy <prateekobireddy@192-168-1-103.tpgi.com.au>
2020-02-17 18:24:41 +1100

    Implemented react-router-dom in index.js (had to run nmp install react-router-dom)

---
+++ resources/js/components/Topic.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+const Topic = () => (
+    <div>
+        <h2>Topic</h2>
+    </div>
+);
+
+export default Topic;
--- resources/js/index.js
+++ resources/js/index.js
@@ -1,7 +1,39 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App'
+import App from './components/App';
+import About from './components/About';
+import Topic from './components/Topic';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+
+const Home = () => (
+    <div>
+        <h2>Home</h2>
+    </div>
+);
 
 if (document.getElementById('root')) {
-    ReactDOM.render(<App />, document.getElementById('root'));
+    ReactDOM.render(
+        <BrowserRouter>
+            {/* <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/topics">Topics</Link>
+                </li>
+            </ul> */}
+            <div>
+                <Switch>
+                    <App />
+                    <Route exact path="/" component={App} />
+                    {/* <Route exact path="/about" component={About} />
+                    <Route exact path="/topics" component={Topic} /> */}
+                </Switch>
+            </div>
+        </BrowserRouter>
+
+        , document.getElementById('root'));
 }
